18d88c03d2700ed47bdabf657bc3c3751eea0f98
Kathy Lin <bluebandedgobi9353@gmail.com>
2012-04-06 15:50:56 -0400

    finished Miniboard module

--- board.ml
+++ board.ml
@@ -5,51 +5,70 @@ exception TODO
 module BOARD =
 sig
     type board
-    type index
+    type place
     type color
-    type threat
+    type threats
 
     (* An empty board *)
     val empty: board
 
     (* Given an index, returns whether it is occupied and with which color*)
-    val getIndex: index -> color
+    val getIndex: place -> color
 
-    (* Inserts pieces on the board, returns None if the insert is invalid *)
-    val insert: board -> index -> color -> board option
+    (* Checks if a space exists and is empty *)
+    val isValid: board -> place -> bool
 
-    (* Removes pieces from a board, returns None if the remove is invalid *)
-    val remove: board -> index -> board option
+    (* Inserts pieces on the board *)
+    val insert: board -> place -> color -> board
+
+    (* Removes pieces from a board, returns None if the remove is invalid 
+       This is only used for testing purposes *)
+    val remove: board -> place -> board
 
     (* Given an index, returns a list of neighbors *)
-    val getNeighbors: index -> index list
+    val getNeighbors: place -> place list
 
     (* Checks if the board has a winning configuration *)
     val isWin: board -> bool
 
-    (* Given a board, returns a list of threats *)
-    val returnThreats: board -> threat list
+    (* Given a board, returns all the threats *)
+    val getThreats: board -> threats
     
 end
 
 (* This is a signature for a board that only considers specific neighbors
    For example, a board of only the horizontal relationships *)
-module MiniBoard : BOARD =
+module MiniBoard (BT: boardtype) (size: int) : BOARD =
 struct
-    Open Color
-    type board
-    type index 
-    type color
+    Open Occupied
+    Open Threat
+    Open Boardstuffs
+    type board = occupied list list
+    type place = index
+    type color = occupied
+    type threats = threat list
 
     (* An empty board *)
-    val empty: board
+    let empty = []
+ 
+    (* determines if an attempted insert is valid *)
+    let isValid (i: index) : bool = raise TODO
 
     (* Inserts a piece on the board *)
-    val insert: board -> index -> board
+    let insert (b: board) (i: index) (c: color) : option = raise TODO
 
     (* Removes a piece on the board *)
-    val remove: 
+    let remove (b: board) (i: index) : board = raise TODO
 
+    (* Gets neighbors but only in the direction specified by BT *)
+    let getNeighbors (i: index) : index list = raise TODO
 
+    (* Checks if there is a win in the direction specified by BT *)
+    let isWin (b: board) : bool = raise TODO
+
+    (* Gets the threats in the direction specified *)
+    let getThreats (b: board) : threats = raise TODO
+
+    (* *)
     
 end
