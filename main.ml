0de7d2bb09bb276a5e60b52464dd978493aa08e2
Peter Zhang <peter.zhang@college.harvard.edu>
2012-04-06 17:27:10 -0400

    comments

--- main.ml
+++ main.ml
@@ -1,8 +1,14 @@
-(* Main.ml Description:
- * Call draw: Draw will take user input, and return a board
- * Call tree:
+(* Main.ml Description: 						    *
+ * Call draw: Draw will take user input, and return a board                 *
+ * Call board: Find all threats on board from draw; create a list of board, *
+ * threat pairs.							    *
+ * Call on each pair in the list:					    *
+ * (1) gen_threat_tree							    *
+ * (2) evaluate_tree  							    *
+ * from tree. If (2) evaluates to a winning sequence, create new board with *
+ * first move from that sequence, and send board to draw.                   *)
 
-open board
+open Board
 open Tree
 open Draw
 
