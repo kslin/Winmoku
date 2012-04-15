510718009469c41ac1d6ab15c49ab719416fbc97
Kathy Lin <bluebandedgobi9353@gmail.com>
2012-04-15 18:57:36 -0400

    finished convertBack

--- board2.ml
+++ board2.ml
@@ -62,7 +62,7 @@ object (self)
 
     (* Methods *)
 
-	method getIndex (i:index) : occupied option = 
+	method private getIndex (i:index) : occupied option = 
         let (x,y) = i in
             try (Some ((List.nth (List.nth board x) y)#get_value))
                 with Failure "nth"|Invalid_argument "List.nth" -> None
@@ -83,10 +83,23 @@ object (self)
         	|DiagLeft -> 
     	    	let (x,y) = i in
         		if (x + y - 1) <= size then (x + y - 1, y)
-        		else (x + y - 1, y + 1 - (x + y - size))
+        		else (x + y - 1, 1 - x  + size)
+
+    method private convertBack (ci:index) = 
+    	match btype with
+	    	|Horizontal -> ci
+    		|Vertical -> let (x,y) = ci in (y,x)
+    		|DiagRight -> 
+    			let (x,y) = ci in
+    			if x < size then (y, (size-x)+y)
+    			else (x+y-size, y) 
+        	|DiagLeft -> 
+        		let (x,y) = ci in
+        		if x <= size then (x-y+1, y)
+        		else (1+size-y,x+y-size)
 
     method private changeIndex (i:index) (c:occupied) = 
-    	let (x,y) = self#convertIndex i in
+    	let (x,y) = i in
     	let rec changerow row yval = match row with
             |[] -> ()
             |hd::tl -> if yval = y then (hd :> piece)#set_value c
@@ -97,20 +110,24 @@ object (self)
                 else changecol tl (xval + 1) in  
         changecol board 1 
 
-    method insert (i:index) (c: occupied) : bool =
+    (* Given an index and a color, insert the piece and update neighbors *)
+    method insert (ci:index) (c: occupied) : bool =
+    	let i = self#convertIndex ci in
     	match self#getIndex i with
     		|None -> false
     		|Some Unocc -> self#changeIndex i c; 
-    			(self#getPiece i)#
+    			List.iter (fun x -> self#addNeighbors x i) 
+    			            (self#getNeighbors i);  
     			true
     		|_ -> false
 
-    method remove (i: index) : bool =
+    method remove (ci: index) : bool =
+    	let i = self#convertIndex ci in
     	match self#getIndex i with
     		|None -> false
     		|_ -> self#changeIndex i Unocc; true
 
-    method getNeighbors (i:index) = 
+    method private getNeighbors (i:index) = 
     	let (x,y) = i in
     	match self#getIndex i with
     		|None -> []
@@ -135,8 +152,22 @@ object (self)
     	checkNeighbors neighbor_list
 
 
-    method getThreats = raise TODO
+    method getThreats =  raise TODO
 
+
+    (*method private hasNeighbors (i:index) : index list option=  
+    	try (Some (List.find (List.mem i) neighbor_list))
+    	with Not_found -> None*)
+
+    (* If n1 is already part of a neighbor list, add n2 to the list.
+       If not, make the two into a new neighbor list *)
+    method private addNeighbors (n1: index) (n2:index) =
+    	let rec findneighlist lst =
+    		match lst with 
+    			|[] -> (n1::n2::[])::neighbor_list
+    			|hd::tl -> if List.mem n1 hd then (n2::hd)::tl
+    				else hd::(findneighlist tl)
+    	in neighbor_list <- (findneighlist neighbor_list)
 
 
 end
