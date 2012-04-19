40f49275efa0b91c1af11cc0c9838b2b3165a4b6
Kathy Lin <bluebandedgobi9353@gmail.com>
2012-04-19 16:34:18 -0400

    miniboard

---
+++ horizontalboard.ml
@@ -0,0 +1,26 @@
+(*open Miniboard*)
+open Boardstuffs
+open Piece
+
+class horizontal_board (bt: boardtype) (size:int): board_object =
+object (self)
+	inherit miniboard size as super
+
+    (*method buildEmptyBoard = 
+        let rec build_board n b = match n with
+            |0 -> b
+            |_ -> (let rec build_row m r = match m with
+                |0 -> r
+                |_ -> build_row (m-1) ((new piece Unocc)::r)
+                in build_board (n-1) ((build_row size [])::b ) )
+        in (build_board size [])*)
+
+    method buildRows = 
+        let rec build_rows n b = match n with
+            |0 -> b
+            |_ -> (let rec build_row m r = match m with
+                |0 -> r
+                |_ -> build_row (m-1) ((n-1,m-1)::r)
+                in build_rows (n-1) ((build_row size [])::b ) )
+        in (build_rows size [])
+end
