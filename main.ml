d5234876dd937d5fa42b19da3b4c801c578fe144
Ina Chen <weilinchen@college.harvard.edu>
2012-04-28 12:18:55 -0400

    umm

--- main.ml
+++ main.ml
@@ -72,22 +72,30 @@ let evaluate_board board =
       
 
 (*  button for eval function *)
-let debug_button_eval () =
+let button_eval () =
   Graphics.set_color Graphics.red;
   Graphics.moveto  (obj_width) ((world_size+6) * obj_width);
   Graphics.draw_string "Debug eval";
   Graphics.fill_rect obj_width ((world_size+5) * obj_width) (2 * obj_width) (obj_width)
 
-let debug_button_threat () =
+let button_threat () =
   Graphics.set_color Graphics.blue;
   Graphics.moveto  (obj_width *5) ((world_size+6) * obj_width);
   Graphics.draw_string "Show Threats";
   Graphics.fill_rect (obj_width*5) ((world_size+5) * obj_width) (2 * obj_width) (obj_width)
 
+let button_playalgo () = () (*
+  Graphics.set_color Graphics.blue;
+  Graphics.moveto  (obj_width *5) ((world_size+6) * obj_width);
+  Graphics.draw_string "Show Threats";
+  Graphics.fill_rect (obj_width*5) ((world_size+5) * obj_width) (2 * obj_width) (obj_width) *)
+
+
 (* Shows buttons and other displays for function testing purposes *)
 let debug_board () = 
-  debug_button_eval ();
-  debug_button_threat ()
+  button_eval ();
+  button_threat ();
+  button_playalgo ()
 
 let rec play_sequence b tlist =
   let rec insertwlist b ilist = 
@@ -122,19 +130,23 @@ let print_gainlist_screen glist =
 (* A handles clicks to to run functions in the area above the board: 
   debugging function, change piece color *)
 let respond_click_header (b:Myboard.board) ((x,y):int*int) = 
+  (* runs eval board function *)
   if ((x > obj_width) && (x < 3 * obj_width) && (y > ((world_size+5) * obj_width)) 
     && (y < ((world_size+6) * obj_width)))
   then (let result = evaluate_board b in
         match result with
         | None -> (print_string "None"; flush_all();)
         | Some tlist -> print_threatlist tlist)
+  (* shows current threats on the game display *)
   else if ((x > obj_width * 5) && (x < 7 * obj_width) && (y > ((world_size+5) * obj_width)) 
     && (y < ((world_size+6) * obj_width)))
   then ((print_string "button working";flush_all());
     (displaythreats := true);)
+  (* changes player to white *)
   else if ((x > obj_width) && (x < 2 * obj_width) && (y > ((world_size+3) * obj_width)) 
     && (y < ((world_size+4) * obj_width)))
   then ((piece_color := White))
+  (* changes player to black *)
   else if ((x > (2 *obj_width)) && (x < 3 * obj_width) && (y > ((world_size+3) * obj_width)) 
     && (y < ((world_size+4) * obj_width))) 
   then ((piece_color := Black))
