f750c94e6df16d00c6f9328a398dccef2922e8f4
Kathy Lin <bluebandedgobi9353@gmail.com>
2012-05-01 13:37:40 -0400

    threats

--- main_compete.ml
+++ main_compete.ml
@@ -63,6 +63,12 @@ let debug_board () =
    tells you to. *)
 let next_move (b:Myboard.board) : int*int =
   (Random.self_init ();
+  match BThreats.evaluate_board b with
+  | Some tlist -> (let Threat(_,tgain, _, _) = 
+                     List.nth tlist ((List.length tlist) - 1) 
+                   in
+                     tgain)
+  | None -> 
     (match Myboard.nextWin b with
     | Some i -> i
     | None -> 
